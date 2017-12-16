02452020bae10773086be7f79992c731781cfa48
Hiroaki Takisawa <takisawa@hit-start.jp>
2017-12-16 09:50:46 +0900

    Add uule encode logic.

--- lib/uule.rb
+++ lib/uule.rb
@@ -1,6 +1,13 @@
 require "uule/version"
+require 'base64'
 
 module Uule
+
+  SPECIAL_KEY_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+
+  # see: https://moz.com/ugc/geolocation-the-ultimate-tip-to-emulate-local-search
   def self.encode(area_name)
+    key = SPECIAL_KEY_TABLE[area_name.size]
+    "w+CAIQICI#{key}#{Base64.strict_encode64(area_name)}"
   end
 end
--- spec/uule_spec.rb
+++ spec/uule_spec.rb
@@ -1,4 +1,7 @@
 RSpec.describe Uule do
   describe '.encode' do
+    it do
+      expect(Uule.encode('Sapporo,Hokkaido,Japan')).to eq 'w+CAIQICIWU2FwcG9ybyxIb2trYWlkbyxKYXBhbg=='
+    end
   end
 end
